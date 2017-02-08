cf4f243589cd3122f57cbcd62a3f25b5e54426b1
guowenfh <chenguowen@raycloud.com>
2017-02-08 23:51:47 +0800

    思考到底要写什么东西🤔

--- src/views/details/index.js
+++ src/views/details/index.js
@@ -5,7 +5,21 @@ class TopicsDetail extends Component {
   constructor(props) {
     super(props);
     this.state ={
-      content :''
+      'id': '',
+      'author_id': '',
+      'tab': '',
+      'title': '',
+      'last_reply_at': '',
+      'good': false,
+      'top': false,
+      'reply_count': 0,
+      'visit_count': 15,
+      'create_at': '',
+      'author': {
+        'loginname': 'yakczh',
+        'avatar_url': 'https://avatars.githubusercontent.com/u/6591466?v=3&s=120'
+        },
+      'is_collect': false
     }
   }
 
@@ -14,7 +28,21 @@ class TopicsDetail extends Component {
       api:'get_topic_details',
       url:'/'+this.props.params.id
     }).then(res=>{
-      // console.error(res.data);
+      this.setState({
+        'id' : res.data.id,
+        'author_id' : res.data.author_id,
+        'tab' : res.data.tab,
+        'title' : res.data.title,
+        'last_reply_at' : res.data.last_reply_at,
+        'good' : res.data.good,
+        'top' : res.data.top,
+        'reply_count' : res.data.reply_count,
+        'visit_count' : res.data.visit_count,
+        'create_at' : res.data.create_at,
+        'author' : res.data.author,
+        'is_collect' : res.data.is_collect
+      })
+      console.error(res.data);
       this.refs.detailContent.innerHTML = res.data.content;
     }).catch(e=>{
       console.error(e);
@@ -26,9 +54,16 @@ class TopicsDetail extends Component {
   render() {
     return (
       <div>
-
-      details {this.props.params.id}
-      <div ref="detailContent"></div>
+        <div style={{'borderBottom':'1px solid #eee'}}>
+          <h1><em>{this.state.top?'置顶':''}</em>{this.state.title}</h1>
+          <ul>
+            <li>发布时间：{this.state.create_at}</li>
+            <li>作者:{this.state.author.loginname}</li>
+            <li>浏览次数{this.state.visit_count}</li>
+            <li>来自{this.state.tab}</li>
+          </ul>
+        </div>
+        <div ref="detailContent"></div>
       </div>
     );
   }
