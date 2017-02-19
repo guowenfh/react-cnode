e0d77d6f92f65f6e82ca78097e7494efe49a87e1
guowenfh <chenguowen@raycloud.com>
2017-02-19 14:05:24 +0800

    关于动态生成 react 组件片段的尝试

--- src/views/message/index.js
+++ src/views/message/index.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 
 class Message extends Component {
-  render() {
-    return (
-      <div>
-        Message
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      reactJson:['div',null]
+    }
+  }
+  componentWillMount(){
+    // this.setState({
+    //   reactJson:['div',{className:'ez-led'},'Hello, React!',React.createElement("div",{className:"ez-led"},"Hello, React!")]
+    // })
+  }
+  render(){
+      console.error(this.state.reactJson);
+      var data = this.state.reactJson;
+      return (
+        <div>
+          <buttom onClick={()=>{
+            var that = this;
+            this.setState({
+              reactJson:['div',{className:'ez-led',onClick:eval('()=>{that.setState({reactJson:["div",null]})}')},'Hello, React!',
+              React.createElement("div",{className:"ez-led"},"Hello, React!")]
+            })
+          }}>
+            按钮
+          </buttom>
+          {React.createElement.apply(null,data)}
+        </div>
+      )
   }
 }
 
