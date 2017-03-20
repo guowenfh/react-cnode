07eab1693fab3f80c35b7ad4fd8b3e2179859ab0
guowenfh <chenguowen@raycloud.com>
2017-03-20 09:47:32 +0800

    postcss

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -52,26 +52,19 @@ var config = {
       __DEV__: JSON.stringify(__DEV__)
     }),
     new webpack.HotModuleReplacementPlugin(),
-    // new webpack.NamedModulesPlugin(),
-    // new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.LoaderOptionsPlugin({
       options: {
         context: '/',
         postcss: [
           autoprefixer({
             browsers: [
-              'ie >= 9',
-              'ie_mob >= 10',
-              'ff >= 30',
-              'chrome >= 34',
-              'safari >= 7',
-              'opera >= 23',
-              'ios >= 7',
-              'android >= 4.4',
-              'bb >= 10'
+              'last 10 versions',
+              'Android >= 4.0',
+              'ios > 7'
             ]
-          }),
-          require('postcss-flexibility')
+          })
         ]
       }
     })
